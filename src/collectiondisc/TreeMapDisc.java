6fed27a00c070ac4ba89623a723ef7cad11f4052
= <=>
2023-04-13 19:56:02 +0530

    Collection

--- src/collectiondisc/TreeMapDisc.java
+++ src/collectiondisc/TreeMapDisc.java
@@ -9,6 +9,7 @@ public class TreeMapDisc {
 TreeMap<String, String> hm = new TreeMap<String, String>();
 		
 		hm.put("Rupee", "India");
+		
 		hm.put("Dollar", "USA");
 		hm.put("Pound", "UK");
 		hm.put("Bhat", "Thailand");
